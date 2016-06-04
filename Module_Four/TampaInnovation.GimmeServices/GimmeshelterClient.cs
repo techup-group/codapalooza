7a98e5bef01c33510fe676697c5bcf79c4f13483
nomad86 <landru86@gmail.com>
2016-06-04 11:01:21 -0400

    Added more api calls

--- Module_Four/TampaInnovation.GimmeServices/GimmeshelterClient.cs
+++ Module_Four/TampaInnovation.GimmeServices/GimmeshelterClient.cs
@@ -6,13 +6,66 @@ namespace TampaInnovation.GimmeServices
 {
     public class GimmeshelterClient
     {
-        private string _baseUrl = " http://api.gimmeshelter.us/gimmeshelter/";
+        private string _baseUrl = "http://api.gimmeshelter.us/gimmeshelter/";
 
         public string GetProviders(string timeStamp, string key, string sig)
         {
-            var jsonQueryString = "{'ts': '"+ timeStamp+"', 'sig': '" + sig + "', 'key': '"+ key+ "'}" ;
-            var url = _baseUrl + "gimmeshelter/Providers?json=" + jsonQueryString;
+            var url = _baseUrl + "gimmeshelter/Providers?json=" + CreateJsonQueryString(timeStamp, key, sig);
             return CallApiGet(url);
+        }
+
+        public string GetContactNumbers(string timeStamp, string key, string sig)
+        {
+            var url = _baseUrl + "gimmeshelter/getContactNumbers?json=" + CreateJsonQueryString(timeStamp, key, sig);
+            return CallApiGet(url);
+        }
+
+        public string GetAddress(string timeStamp, string key, string sig)
+        {
+            var url = _baseUrl + "gimmeshelter/Providers/getAddress?json=" + CreateJsonQueryString(timeStamp, key, sig);
+            return CallApiGet(url);
+        }
+
+        public string GetAreasServed(string timeStamp, string key, string sig)
+        {
+            var url = _baseUrl + "gimmeshelter/Providers/getAreasServed?json=" + CreateJsonQueryString(timeStamp, key, sig);
+            return CallApiGet(url);
+        }
+
+        public string GetServices(string timeStamp, string key, string sig)
+        {
+            var url = _baseUrl + "gimmeshelter/Providers/getServices?json=" + CreateJsonQueryString(timeStamp, key, sig);
+            return CallApiGet(url);
+        }
+
+        public string GetGeograpphy(string timeStamp, string key, string sig, string zipCode)
+        {
+            var url = _baseUrl + "gimmeshelter/Providers/getGeography?json=" + CreateJsonQueryStringWithZipCode(timeStamp, key, sig, zipCode);
+            return CallApiGet(url);
+        }
+
+        public string GetServicesGeograpphy(string timeStamp, string key, string sig, string zipCode)
+        {
+            var url = _baseUrl + "gimmeshelter/Providers/getServicesGeography?json=" + CreateJsonQueryStringWithZipCode(timeStamp, key, sig, zipCode);
+            return CallApiGet(url);
+        }
+
+        public string GetBedUnitInventory(string timeStamp, string key, string sig)
+        {
+            var url = _baseUrl + "gimmeshelter/Providers/getBedUnitInventory?json=" + CreateJsonQueryString(timeStamp, key, sig);
+            return CallApiGet(url);
+        }
+
+   
+        #region Private Methods
+        private string CreateJsonQueryString(string timeStamp, string key, string sig)
+        {
+            return "{\"ts\": \"" + timeStamp + "\", 'sig\": \"" + sig + "\", \"key\": \"" + key + "\"}";
+        }
+
+        private string CreateJsonQueryStringWithZipCode(string timeStamp, string key, string sig, string zipCode)
+        {
+            return "{\"ts\": \"" + timeStamp + "\", 'sig\": \"" + sig + "\", \"key\": \"" + key + "\", \"zip}\": \"" + zipCode + "\"}";
         }
 
         private string CallApiGet(string url)
@@ -30,9 +83,11 @@ namespace TampaInnovation.GimmeServices
                     throw new Exception("Not found");
 
                 string msg = response.Content.ReadAsStringAsync().Result;
-                throw new Exception($"The following error occurred while retrieving order data from Api: {msg}");
+                throw new Exception($"The following error occurred while retrieving provider data from Api: {msg}");
             }
 
         }
+
+        #endregion
     }
 }
