1f1220d678c402721acc7e91d3a9832874403d82
Jonathas Sucupira <jonathas@jsucupira.com>
2016-06-04 08:51:30 -0400

    Adding utility class

---
+++ Module_Four/TampaInnovation.Business/Helpers/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TampaInnovation.Business.Helpers
+{
+    public class Utilities
+    {
+        public static double CurrentUnixTimeStamp(DateTime dateTime)
+        {
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return dateTime.Subtract(epoch).TotalSeconds;
+        }
+    }
+}
