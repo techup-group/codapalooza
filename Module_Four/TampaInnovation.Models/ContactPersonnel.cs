4d5ac39b64160ceb936739c7a032e6d8870d4f61
Jonathas Sucupira <jonathas@jsucupira.com>
2016-06-04 15:25:24 -0400

    Changing some of the end-points

--- Module_Four/TampaInnovation.Models/ContactPersonnel.cs
+++ Module_Four/TampaInnovation.Models/ContactPersonnel.cs
@@ -5,10 +5,6 @@ namespace TampaInnovation.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string Title { get; set; }
-        public string TelephoneAreaCode { get; set; }
-        public string TelephoneExtension { get; set; }
-        public string TelephoneLine { get; set; }
-        public string TelephonePrefix { get; set; }
         public string Number { get; set; }
     }
 }
